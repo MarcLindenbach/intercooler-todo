c546d79a0333defaa6daf1a1f9f6d2db6911634e
Marc Lindenbach <marc.lindenbach@gmail.com>
2016-08-29 19:25:56 -0600

    finished basic todo

---
+++ server.py
@@ -0,0 +1,76 @@
+import os.path
+from flask import Flask, render_template, request
+import json
+app = Flask(__name__)
+
+JSON_FILE = 'data.json'
+
+def read_todos():
+    if not os.path.exists(JSON_FILE):
+        return []
+
+    with open(JSON_FILE, 'r+') as data_file:
+        todos = json.load(data_file)
+    return todos
+
+def save_todos(todos):
+    with open(JSON_FILE, 'w+') as data_file:
+        json.dump(todos, data_file)
+
+def find_todo(todos, id):
+    for i in range(len(todos)):
+        if todos[i]['id'] == int(id):
+            return i
+    return -1
+
+@app.route('/')
+def index():
+    todos = read_todos()
+    return render_template('index.html', todos=todos)
+
+@app.route('/todos', methods=['GET'])
+def todos_get():
+    todos = read_todos()
+    return render_template('todos.html', todos=todos)
+
+@app.route('/todos/', methods=['POST'])
+def todos_post():
+    if request.form['text'] == '':
+        return ('', 200)
+
+    todos = read_todos()
+
+    next_id = 1
+    if len(todos) > 0:
+        next_id = todos[-1]['id'] + 1
+
+    todos.append({
+        'id': next_id,
+        'text': request.form['text']
+    })
+
+    save_todos(todos)
+    return render_template('todo_form.html')
+
+@app.route('/todos/<id>', methods=['DELETE'])
+def todos_delete(id):
+    todos = read_todos()
+    todos.pop(find_todo(todos, id))
+
+    save_todos(todos)
+    return('', 200)
+
+@app.route('/todos/<id>', methods=['PUT'])
+def toggle_todo(id):
+    todos = read_todos()
+    i = find_todo(todos, id)
+    if 'complete' in todos[i]:
+        todos[i]['complete'] = not todos[i]['complete']
+    else:
+        todos[i]['complete'] = True
+
+    save_todos(todos)
+    return('', 200)
+
+if __name__ == '__main__':
+    Flask.run(app)
